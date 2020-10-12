649714a0601f588ff080cb44e002a799f613bac3
tegarsweefer <tegar.s@weefer.co.id>
2020-10-12 10:59:42 +0700

    first commit

---
+++ ExampleTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerService1
+{
+    public class ExampleTask
+    {
+        public void task1()
+        {
+            //doing somehting like call webhook, call api, and other
+            Console.WriteLine("Hallo...");
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WorkerService1
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                ExampleTask exampleTask = new ExampleTask();
+                exampleTask.task1();
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
